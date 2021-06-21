33fcf0b88a23ef6142b6ff4cab31e788f8a2c48a
simon <simon.hobbs@electrooptical.net>
2021-06-21 13:13:26 -0400

    reorganizing

---
+++ python/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(name='HP894xx',
+      version='1.0.0',
+      description='HP894xx Series GPIB Driver',
+      url='',
+      author='ElectroOptical Innovations, LLC.',
+      author_email='simon.hobbs@electrooptical.net',
+      license='BSD',
+      packages=find_packages(),
+      install_requires=[
+          'prologix_gpib_bridge',
+          'click',
+          'pyserial',
+          'numpy',
+          'timeout_decorator'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=[],
+      include_package_data=True,
+      zip_safe=True)
